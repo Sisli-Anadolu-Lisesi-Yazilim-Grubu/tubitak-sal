6172d32c0fad0fa9602db5d8b1c2512ab9c845bd
Samet 732 <samethub732@gmail.com>
2020-12-06 20:46:36 +0300

    First Production

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>Tübitak ŞAL</p>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainPage from './components/MainPage';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <MainPage />
+    </React.StrictMode>,
+    document.getElementById('root')
+);

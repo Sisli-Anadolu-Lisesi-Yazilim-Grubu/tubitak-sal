91a552d24e9245d9091f1e0333817db350de810d
Samet 732 <samethub732@gmail.com>
2020-12-12 19:40:26 +0300

    Created Navbar

--- src/components/ProjectSelector.js
+++ src/components/ProjectSelector.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class ProjectSelector extends React.Component {
-    render () {
+    render() {
         return (
             <div>
-                {this.props.year + '/' + this.props.type}
+                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                    <a href='/'><button className="btn"><span style={{color: "#00005f", fontSize: "3rem"}}>&lang;</span></button></a>
+                    <a className="navbar-brand" href="/">Tübitak ŞAL</a>
+                </nav>
             </div>
         );
     }

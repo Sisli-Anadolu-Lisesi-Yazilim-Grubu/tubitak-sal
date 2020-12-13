25cb38a37fb76704559ae446e3ffdb5c89694d13
Samet 732 <samethub732@gmail.com>
2020-12-13 20:14:06 +0300

    Project Selector 80% completed

--- src/components/ProjectSelector.js
+++ src/components/ProjectSelector.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import './ProjectSelector.css';
+import { filterProjects } from './../app/store_functions';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 class ProjectSelector extends React.Component {
-    render() {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            projects: filterProjects(this.props.projects, this.props.year, this.props.type)
+        };
+    }
+
+    render () { //TODO: add search and image preview
         return (
             <div>
-                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-                    <a href='/'><button className="btn"><span style={{color: "#00005f", fontSize: "3rem"}}>&lang;</span></button></a>
-                    <a className="navbar-brand" href="/">Tübitak ŞAL</a>
-                </nav>
+                <Navbar href="/" />
+                <main className="project-selector">
+                    <ul className="project-preview-list">
+                        {this.state.projects.map((val, i) => (
+                            <Link key={i} to={`/projects/${this.props.year}/${this.props.type}/${val.hash}`}>
+                                <li className="project-preview">{val.title}</li>
+                            </Link>
+                        ))}
+                    </ul>
+                </main>
+                <Footer />
             </div>
         );
     }
